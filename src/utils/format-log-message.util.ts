bf3043aff2954ff2e4ea7717f0efcf7a7a5daab3
webcoderspeed <webcoderspeed@gmail.com>
2024-05-06 22:46:38 +0530

    fix: log format

--- src/utils/format-log-message.util.ts
+++ src/utils/format-log-message.util.ts
@@ -4,12 +4,18 @@ function formatLogMessage(message: string, data?: unknown) {
   const TRACE_ID = TraceIdHandler.getTraceIdField();
   if (data && typeof data === 'object' && TRACE_ID in data) {
     const { [TRACE_ID]: traceId, ...restData } = data as { [key: string]: any };
-    const stringifiedData = JSON.stringify({ message, data: restData });
+    const stringifiedData =
+      Object.keys(restData)?.length > 0
+        ? JSON.stringify({ message, data: restData })
+        : JSON.stringify({ message });
     return `${traceId}:${stringifiedData}`;
   }
 
-  const logData = data ? { data } : {};
-  return JSON.stringify({ message, data: logData });
+  const logData = data && Object.keys(data)?.length > 0 ? { data } : {};
+  return JSON.stringify({
+    message,
+    ...(Object.keys(logData).length > 0 && { data: logData }),
+  });
 }
 
 export default formatLogMessage;
